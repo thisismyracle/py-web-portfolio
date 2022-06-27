82502afbedb023cb9c5888c5ce19bf459188e24d
Myracle <99720260+thisismyracle@users.noreply.github.com>
2022-06-27 13:41:15 +0700

    Adds runtime.txt

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"thisismyracle@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
